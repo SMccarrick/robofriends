b1adc05564b33075d8d6b63a97943257d38a914c
housepartystevee <smccarrick2804@gmail.com>
2019-02-28 11:20:44 +1300

    updated components

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -3,7 +3,7 @@ import Card from './Card';
 
 const CardList = ({ robots }) => {
 	return (
-		<div>
+		<div> 
 			{robots.map((user, i) => {
 				return <Card key={i} id={robots[i].id} name={robots[i].name} email={robots[i].email} />;
 			})}
--- src/containers/App.js
+++ src/containers/App.js
@@ -37,14 +37,14 @@ class App extends Component {
 		return !robots.length ? (
 			<h1>Loading</h1>
 		) : (
-			<div className="tc">
-				<h1 className="f1">RoboFriends</h1>
-				<SearchBox searchChange={this.onSearchChange} />
-				<Scroll>
-					<CardList robots={filteredRobots} />
-				</Scroll>
-			</div>
-		);
+				<div className="tc">
+					<h1 className="f1">RoboFriends</h1>
+					<SearchBox searchChange={this.onSearchChange} />
+					<Scroll>
+						<CardList robots={filteredRobots} />
+					</Scroll>
+				</div>
+			);
 	}
 }
 
